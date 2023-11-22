423517e9ab511ae78de1f835fd81b01d7f271615
Caleb Mak <calebmak@gmail.com>
2023-11-21 21:04:26 -0800

    made the private variables and made the header of the constructor

--- header/task.h
+++ header/task.h
@@ -3,8 +3,20 @@
 
 #include <iostream>
 #include <string>
+#include "Time.h"
 using namespace std;
 
+class Task {
+private:
+    Time start_time;
+    Time end_time;
+    string category;
+    string location;
+    int priority;
 
+public:
+    Task(Time _start, Time _end, string _catagory, string _location, int _priority);
+
+};
 
 #endif
