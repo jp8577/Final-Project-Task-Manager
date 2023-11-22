733767839b614be77562ffdbecd207493d4f20c3
Caleb Mak <calebmak@gmail.com>
2023-11-21 22:07:58 -0800

    write the implimentation for the time class

---
+++ header/time.h
@@ -0,0 +1,26 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Time {
+private:
+    int hours;
+    int minutes;
+    bool pm;
+    bool isValidHours(int hour);
+    bool isValidMins(int min);
+
+public:
+    Time(int _hours, int _minutes, bool _pm);
+    void setHours(int num);
+    void setMins(int num);
+    void setPm(bool _pm);
+    int getHours();
+    int getMins();
+    bool getPm();
+};
+
+#endif
