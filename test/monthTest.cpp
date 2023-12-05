635db633e44f204b307d160ff1e3b8750b600244
jp8577 <jpark8577@gmail.com>
2023-12-04 22:01:55 -0800

    Testing functions for month class

--- test/monthTest.cpp
+++ test/monthTest.cpp
@@ -6,3 +6,38 @@ int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+TEST(MonthTests, monthCheckDaysInMonth1) {
+    
+}
+
+TEST(MonthTests, monthCheckDaysInMonth2) {
+}
+
+TEST(MonthTests, monthCheckDaysInMonth3) {
+    
+}
+
+TEST(MonthTests, checkFirstDay1) {
+    
+}
+
+TEST(MonthTests, checkFirstDay2) {
+    
+}
+
+TEST(MonthTests, checkFirstDay3) {
+    
+}
+
+TEST(MonthTests, isLeap1) {
+    
+}
+
+TEST(MonthTests, isLeap2) {
+    
+}
+
+TEST(MonthTests, isLeap3) {
+    
+}
