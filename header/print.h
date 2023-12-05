e2430f7db94c6b785726ee038900e393eefd67ab
Caleb Mak <calebmak@gmail.com>
2023-12-04 22:36:19 -0800

    implement printTime function in print class

--- header/print.h
+++ header/print.h
@@ -1,14 +1,15 @@
 #ifndef PRINT_H
 #define PRINT_H
-#include "calendar.h"
+#include "../header/calendar.h"
 
 class Print {
     public:
+        Print();
         void printDay(Calendar cal);
         void printMonth(Calendar cal); 
         void printCalendar(Calendar cal); 
         void printTask(Calendar cal); 
-        void printTime(Calendar cal); 
+        void printTime(Time* clock); 
 };
 
 #endif
