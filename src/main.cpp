0442c188273659acc541e0f0c8ed57239dc21e66
Caleb Mak <calebmak@gmail.com>
2023-12-05 19:13:55 -0800

    test printTask

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,13 @@
 #include "print.cpp"
 #include "task.cpp"
 #include "time.cpp"
+
+int main() {
+    Time* start = new Time(10, 0, false);
+    Time* end = new Time(11, 50, false);
+    Task* yuh = new Task("cs class", start, end, "school", "UCR", "learn stuff", 1);
+    Print* printer = new Print();
+    printer->printTask(yuh);
+
+    return 0;
+}
