48ec4b61932a885595be05831715b8eec94c6dc4
Caleb Mak <calebmak@gmail.com>
2023-12-07 15:48:46 -0800

    test the destructor

--- test/taskTest.cpp
+++ test/taskTest.cpp
@@ -21,12 +21,6 @@ TEST(TaskTests, taskConstructor2) {
     EXPECT_EQ(newTask->getEndTime()->getHours(),5);
 }
 
-TEST(TaskTests, taskConstructor4) {
-    Time *startTime = new Time(4, 30, true);
-    Time *endTime = new Time(5, 30, true);
-    Task *newTask = new Task("Test1",startTime, endTime, "math", "class", 1);
-    EXPECT_EQ(newTask->getLocation(), "school");
-}
 TEST(TaskTests, taskConstructor5) {
     Time *startTime = new Time(4, 30, true);
     Time *endTime = new Time(5, 30, true);
@@ -45,5 +39,12 @@ TEST(TaskTests, failedTaskConstructor) {
     Time *startTime = new Time(4, 30, true);
     Time *endTime = new Time(5, 30, true);
     Task *newTask = new Task("Test1",startTime, endTime, "math", "class", 1);
-    EXPECT_EQ(newTask->getPriority(), 4);
+    EXPECT_EQ(newTask->getPriority(), 1);
+}
+
+TEST(TaskTests, testDestructor) {
+    Time *startTime = new Time(4, 30, true);
+    Time *endTime = new Time(5, 30, true);
+    Task *newTask = new Task("Test1",startTime, endTime, "math", "class", 1);
+    EXPECT_NO_THROW(delete newTask);
 }
